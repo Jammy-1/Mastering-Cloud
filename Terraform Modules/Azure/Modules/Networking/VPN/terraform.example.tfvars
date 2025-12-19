6b85bf6a7e94544047687ce825a0cf69d5b11cdc
Jammy-1 <jplested132@gmail.com>
2025-12-19 20:26:20 +0000

    - VPN Module

---
+++ Terraform Modules/Azure/Modules/Networking/VPN/terraform.example.tfvars
@@ -0,0 +1,13 @@
+# VPN
+vpn_gateway_name            = "vpn-gateway-SSI"
+public_ip_allocation_method = "Dynamic"
+public_ip_sku               = "Basic"
+
+network_vpn_gateway_type = "Vpn"
+vpn_gateway_type         = "RouteBased"
+vpn_sku                  = "VpnGw1"
+
+on_prem_gateway_name = "on-prem-gateway"
+
+vpn_name = "Site-To-Site-Connection"
+vpn_type = "IPsec"
---
+++ Terraform Modules/Azure/Modules/Networking/VPN/variables.tf
@@ -0,0 +1,31 @@
+# General
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "tags" { type = map(string) }
+
+#Public IP
+variable "public_ip_allocation_method" { type = string }
+variable "public_ip_sku" { type = string }
+variable "public_ip_name" { type = string }
+
+#VPN Gateway
+variable "vpn_gateway_name" { type = string }
+variable "network_vpn_gateway_type" { type = string }
+variable "vpn_gateway_type" { type = string }
+variable "vpn_sku" { type = string }
+
+# On Prem Gateway
+variable "on_prem_gateway_name" { type = string }
+variable "on_prem_public_ip" { type = string }
+variable "on_prem_address_space" { type = string }
+
+# VPN Connection Settings
+variable "vpn_name" { type = string }
+variable "vpn_type" { type = string }
+variable "vpn_shared_key" {
+  type      = string
+  sensitive = true
+}
+
+# Subnet
+variable "gateway_subnet" { type = string }
